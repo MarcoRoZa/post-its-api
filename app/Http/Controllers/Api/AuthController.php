2811b76fa3c17bfd80a736d249b165d51f629498
Kevin Rodriguez <kegi.rodriguezz@gmail.com>
2022-08-26 01:52:51 -0500

    Adding doc for /api/register

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -8,6 +8,9 @@ use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
+/**
+ * @OA\Info(title="Autenticación", version="1.0")
+ */
 class AuthController extends Controller
 {
     private $apiTokenService;
@@ -17,6 +20,36 @@ class AuthController extends Controller
         $this->apiTokenService = $tokenService;
     }
 
+    /**
+     * @OA\Post(
+     *      path="/api/register",
+     *      summary="Registrar un usuario",
+     *      @OA\RequestBody(
+     *          @OA\MediaType(
+     *              mediaType="application/json",
+     *              @OA\Schema(
+     *                  @OA\Property(
+     *                      property="name",
+     *                      type="string"
+     *                  ),
+     *                  @OA\Property(
+     *                      property="email",
+     *                      type="string"
+     *                  ),
+     *                  @OA\Property(
+     *                      property="password",
+     *                      type="string"
+     *                  ),
+     *                  example={"name": "Marco", "email": "marco@test.com", "password": "123456"}
+     *              )
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Retorna el token generado."
+     *      ),
+     * )
+     */
     public function register(Request $request)
     {
         User::query()->create([
