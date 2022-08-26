ad7869a292db93b363363883015af65228484c79
Kevin Rodriguez <kegi.rodriguezz@gmail.com>
2022-08-26 02:00:59 -0500

    Adding test when a user can be signed in

--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\User;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class AuthenticationTest extends TestCase
@@ -10,6 +12,21 @@ class AuthenticationTest extends TestCase
     {
         $response = $this->postJson('/api/register', [
             'name' => "Marco",
+            'email' => "marco@test.com",
+            'password' => "123456",
+        ]);
+
+        $response->assertOk();
+    }
+
+    public function testAUserCanBeLoggedIn()
+    {
+        factory(User::class)->create([
+            'email' => "marco@test.com",
+            'password' => Hash::make("123456"),
+        ]);
+
+        $response = $this->postJson('/api/login', [
             'email' => "marco@test.com",
             'password' => "123456",
         ]);
