a0bd68aa9ce0d241902f659a82b33a280bce65b7
Kevin Rodriguez <kegi.rodriguezz@gmail.com>
2022-08-26 18:16:30 -0500

    Adding test when a user can join to existing group

--- tests/Feature/GroupTest.php
+++ tests/Feature/GroupTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\Group;
+use App\Models\GroupUser;
 use App\User;
 use Laravel\Passport\Passport;
 use Tests\TestCase;
@@ -17,5 +18,19 @@ class GroupTest extends TestCase
 
         $response->assertOk();
         $response->assertJsonCount(Group::all()->count());
+    }
+
+    public function testAUserCanJoinToExistingGroup()
+    {
+        $user = factory(User::class)->create();
+        Passport::actingAs($user);
+
+        $group = Group::query()->firstOrFail();
+
+        $response = $this->getJson("/api/groups/$group->uuid/join");
+
+        $response->assertOk();
+        $response->assertJsonStructure(['notes']);
+        $this->assertNotEmpty(GroupUser::query()->where('user_id', $user->id)->where('group_id', $group->id)->get());
     }
 }
